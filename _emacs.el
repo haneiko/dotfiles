df75b4f2835b2379c7e77ba0ee02e6f68046d4af
Mateus C. Haneiko <mateus.haneiko@gmail.com>
2017-09-15 00:34:43 -0300

    freebsd config

---
+++ _emacs.el
@@ -0,0 +1,255 @@
+;;; emacs.el --- my emacs config
+;;; Commentary:
+;;; Code:
+
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+;;(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+;;(add-to-list 'package-archives
+	     ;;'("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(setq inhibit-startup-screen t) ; Disables the manual page on startup
+(column-number-mode 1) ; Show column number in the mode line
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(when (boundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(show-paren-mode 1) ; highlight matching paren, brace
+(electric-pair-mode 1) ; auto-pair ({[]})
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
+(add-hook 'text-mode 'turn-on-auto-fill)
+(setq package-enable-at-startup nil)
+
+(defun server-remove-kill-buffer-hook ()
+  "Disable the prompt for killing emacsclient buffers."
+  (remove-hook 'kill-buffer-query-functions
+	       'server-kill-buffer-query-function))
+(add-hook 'server-visit-hook 'server-remove-kill-buffer-hook)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; use a mix of tabs to indent and spaces to align
+(use-package smart-tabs-mode
+  :ensure t
+  :config
+  (smart-tabs-insinuate 'c 'c++))
+
+(use-package evil-leader
+  :ensure t
+  :config
+  (global-evil-leader-mode)
+  (evil-leader/set-leader ",")
+  (evil-leader/set-key
+    ","  'other-window
+    "r"  'evil-edit
+    "c"  'comment-dwim
+    "e"  'find-file
+    "i"  'indent-region
+    "d"  'kill-this-buffer
+    "s"  'eshell
+    "g"  'magit-status
+    "l"  'whitespace-mode       ;; Show invisible characters
+    "o"  'delete-other-windows  ;; C-w o
+    "w"  'save-buffer
+    "k"  'goto-line))
+
+(use-package evil ; vim plugin
+  :ensure t
+  :config
+  (evil-mode 1))
+
+(use-package paredit ; lisp plugin
+  :ensure t
+  :config
+  (add-hook 'emacs-lisp-mode-hook       'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook             'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+  (add-hook 'scheme-mode-hook           'enable-paredit-mode)
+  (use-package evil-paredit
+    :ensure t
+    :config
+    (add-hook 'emacs-lisp-mode-hook       'evil-paredit-mode)
+    (add-hook 'lisp-mode-hook             'evil-paredit-mode)
+    (add-hook 'lisp-interaction-mode-hook 'evil-paredit-mode)
+    (add-hook 'scheme-mode-hook           'evil-paredit-mode)))
+
+(use-package paredit-everywhere
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook        'paredit-everywhere-mode))
+
+(use-package yasnippet
+  :ensure t
+  :config
+  (yas-global-mode 1))
+
+(use-package company ; auto-complete plugin
+  :ensure t
+  :config
+  (global-company-mode 1)
+  (use-package company-jedi ; Python
+    :ensure t
+    :config
+    (setq jedi:environment-virtualenv
+          (list (expand-file-name "~/.emacs.d/.python-environments/")))
+    (add-hook 'python-mode-hook 'jedi:setup)
+    (setq jedi:complete-on-dot t)
+    (setq jedi:use-shortcuts t)
+    (defun config/enable-company-jedi ()
+      (add-to-list 'company-backends 'company-jedi))
+      (add-hook 'python-mode-hook 'config/enable-company-jedi))
+  (use-package company-bibtex
+    :ensure t
+    :config
+    (add-to-list 'company-backends 'company-bibtex))
+  (use-package company-c-headers
+    :ensure t
+    :config
+    (add-to-list 'company-backends 'company-c-headers)
+    (add-to-list 'company-c-headers-path-system
+		 "/usr/include/c++/4.8/")))
+;; only /usr/include/ and /usr/local/include/ by default
+
+(use-package geiser ; scheme plugin
+  :ensure t)
+
+(use-package magit ; git plugin
+  :ensure t
+  :config
+  (global-magit-file-mode 1)
+  (use-package evil-magit
+    :ensure t))
+
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-mode 1)
+  (setq projectile-enable-caching t))
+
+(use-package web-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (setq web-mode-enable-auto-closing t)
+  (setq web-mode-enable-auto-pairing t)
+  (setq-default indent-tabs-mode nil))
+
+;; (use-package js2-mode
+;;   :ensure t
+;;   :config
+;;   (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+;; (use-package js2-refactor
+;;   :ensure t
+;;   :config
+;;   (add-hook 'js2-mode-hook #'js2-refactor-mode))
+
+;; (use-package auto-complete
+;;   :ensure t
+;;   :config
+;;   (global-auto-complete-mode))
+;; (use-package ac-js2
+;;   :ensure t
+;;   :config
+;;   (add-hook 'js2-mode-hook 'ac-js2-mode))
+
+;; JavaScript completion - autocomplete version
+;; npm install tern
+;; (use-package tern
+;;   :ensure t
+;;   :config
+;;   (add-hook 'js-mode-hook (lambda () (tern-mode t)))
+;;   (use-package tern-auto-complete
+;;     :ensure t
+;;     :config
+;;     (tern-ac-setup)))
+
+;; JavaScript completion - company version
+;; npm install -g tern
+;; needs .tern-project file on project folder
+(use-package tern
+  :ensure t
+  :config
+  (add-hook 'js-mode-hook (lambda () (tern-mode t)))
+  (use-package company-tern
+    :ensure t
+    :config
+    (add-to-list 'company-backends 'company-tern)
+    (setq company-tern-property-marker " p")))
+
+;; JavaScript: npm install -g jshint
+(use-package flycheck
+  :ensure t
+  :config
+  (global-flycheck-mode 1)
+  (add-hook 'flycheck-mode-hook
+	    (lambda ()
+	      (evil-define-key
+		'normal flycheck-mode-map (kbd "]e")
+		'flycheck-next-error)
+	      (evil-define-key
+		'normal flycheck-mode-map (kbd "[e")
+		'flycheck-previous-error))))
+
+(add-hook 'c-mode-common-hook`
+	  (lambda ()
+	    (setq c-default-style
+		  '((java-mode . "java")
+		    (awk-mode . "awk")
+		    (other . "k&r")))
+	    ;; underscore will be recognised as word character
+	    (modify-syntax-entry ?_ "w")
+	    ;; set old style comments
+	    (setq comment-style 'extra-line)
+	    (setq tab-width 4)
+	    (setq compilation-read-command nil)
+	    (setq compile-command "make --no-print-directory -C debug")
+	    (setq show-trailing-whitespace t)
+	    ;; on save delete all trailing whitespace
+	    (add-to-list 'write-file-functions
+			 'delete-trailing-whitespace)))
+
+(add-hook 'scheme-mode-hook`
+	  (lambda ()
+	    ;; underscore will be recognised as word character
+	    (modify-syntax-entry ?_ "w")
+	    (setq show-trailing-whitespace t)
+	    ;; on save delete all trailing whitespace
+	    (add-to-list 'write-file-functions
+			 'delete-trailing-whitespace)
+	    (setq geiser-active-implementations '(guile))
+	    (setq geiser-repl-use-other-window nil)
+	    (setq geiser-repl-query-on-kill-p nil)
+	    (geiser-mode 1)))
+
+(add-hook 'latex-mode-hook`
+	  (lambda ()
+	    (turn-on-auto-fill)
+	    (setq compilation-read-command nil)))
+
+(provide 'emacs)
+;;; emacs.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (flycheck company-tern tern web-mode projectile evil-magit magit geiser company-c-headers company-bibtex company-jedi company yasnippet paredit-everywhere evil-paredit paredit evil-leader smart-tabs-mode use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ _vimrc
@@ -0,0 +1,138 @@
+" Don't be vi-compatible
+set nocompatible
+
+set autoindent
+set backspace=indent,eol,start
+set smarttab
+
+" While typing a search command, show where the pattern, as it was typed
+" so far, matches.
+set incsearch
+" Use <C-L> to clear the highlighting of :set hlsearch.
+nnoremap <silent> <C-L> :nohlsearch<CR>
+
+" The last window will always have a status line
+set laststatus=2
+
+" Show the line and column number of the cursor position,
+" separated by a comma
+set ruler
+
+" When 'wildmenu' is on,
+" command-line completion operates in an enhanced mode.
+set wildmenu
+
+" Minimal number of screen lines to keep above and below the cursor.
+set scrolloff=1
+
+" When a file has been detected to have been changed outside of Vim and
+" it has not been changed inside of Vim, automatically read it again.
+set autoread
+
+" A history of ":" commands, and a history of previous search patterns
+" is remembered.
+set history=1000
+
+filetype plugin indent on
+
+" Maximum width of text that is being inserted.
+set textwidth=79
+
+" Print the line number in front of each line.
+set number
+
+" Number of spaces that a <Tab> in the file counts for.
+set tabstop=4
+
+" Number of spaces to use for each step of (auto)indent.
+" Used for |'cindent'|, |>>|, |<<|, etc.
+set shiftwidth=4
+
+" Use real <TAB>s ?
+set noexpandtab
+
+" Don't make a backup before overwriting a file.
+set nobackup
+set nowritebackup
+
+" Don't use a swapfile for the buffer.
+set noswapfile
+
+" Show (partial) command in the last line of the screen.
+set showcmd
+
+" Maximum number of changes that can be undone.
+set undolevels=1000
+
+" Save the whole buffer for undo when reloading it.
+set undoreload=10000
+
+" Don't ring the bell (beep or screen flash) for error messages.
+set noerrorbells
+
+" When on, the ":substitute" flag 'g' is default on.  This means that
+" all matches in a line are substituted instead of one.
+set gdefault
+
+" Ignore case in search patterns. Also used when searching in the tags file.
+set ignorecase
+
+" Override the 'ignorecase' option if the search pattern contains upper
+" case characters.
+set smartcase
+
+" When non-empty, specifies the key sequence that toggles the 'paste' option
+set pastetoggle=<F2>
+
+" When included, Vim will use the clipboard register '*'
+" for all yank, delete, change and put operations which
+" would normally go to the unnamed register.
+set clipboard=unnamed
+
+" Automatic reload of .vimrc
+autocmd! bufwritepost .vimrc source %
+
+" Show trailing whitespace
+highlight extrawhitespace ctermbg=red cterm=none
+autocmd InsertEnter * match extrawhitespace /\s\+\%#\@<!$/
+autocmd InsertLeave * match extrawhitespace /\s\+$/
+
+" Remove trailing whitespace on save for ___ files
+function! s:RemoveTrailingWhitespaces()
+    "Save last cursor position
+    let l = line(".")
+    let c = col(".")
+
+    %s/\s\+$//ge
+
+    call cursor(l,c)
+endfunction
+autocmd BufWritePre * :call <SID>RemoveTrailingWhitespaces()
+
+" Start highlighting, use 'background' to set colors
+syntax on
+
+" Number of colors
+set t_Co=256
+
+"highlight Normal ctermbg=Black ctermfg=White
+
+" When set to "dark", Vim will try to use colors that look good on a
+" dark background.  When set to "light", Vim will try to use colors that
+" look good on a light background.
+"set background=dark
+
+" 'colorcolumn' is a comma separated list of screen columns that are
+" highlighted with ColorColumn |hl-ColorColumn|.
+set colorcolumn=80
+highlight colorcolumn ctermbg=White cterm=none
+
+" Mistakes
+inoremap <F1> <ESC>
+nnoremap <F1> <ESC>
+vnoremap <F1> <ESC>
+
+" Keep selection after indentation
+vnoremap < <gv
+vnoremap > >gv
+
