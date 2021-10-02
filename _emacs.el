ddbfe317d4b1e288f03f04e64c87f018364a225c
Mateus C. Haneiko <mateus.haneiko@gmail.com>
2021-10-02 14:28:19 -0300

    emacs: markdown-mode

--- _emacs.el
+++ _emacs.el
@@ -229,6 +229,14 @@
   :config
   (add-hook 'prog-mode-hook        'paredit-everywhere-mode))
 
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
 (use-package yasnippet
   :ensure t
   :config
